f483e897fb9dba91ac10168f5a62243d096c3614
Jaideep Bose <jaideepbose51@gmail.com>
2025-03-26 02:23:17 +0530

    issue fixed

--- main.py
+++ main.py
@@ -130,6 +130,9 @@ def developer():
 def blog():
     return render_template("blog.html")
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))  # Use PORT from environment, default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
 
